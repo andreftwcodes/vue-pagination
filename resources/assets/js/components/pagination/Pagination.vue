f1848b906c92967de2e6272ee46432bb05b5daf4
Andre Alvarico <alvarico.ad@gmail.com>
2018-09-03 20:15:39 +0800

    2nd commit

--- resources/assets/js/components/pagination/Pagination.vue
+++ resources/assets/js/components/pagination/Pagination.vue
@@ -2,32 +2,32 @@
     <nav aria-label="Page navigation example">
         <ul class="pagination">
             <li class="page-item" :class="{ 'disabled': meta.current_page === 1 }">
-                <a class="page-link" href="#" aria-label="Previous" @click.prevent="swtiched(meta.current_page - 1)">
+                <a class="page-link" href="#" aria-label="Previous" @click.prevent="switched(meta.current_page - 1)">
                     <span aria-hidden="true">&laquo;</span>
                     <span class="sr-only">Previous</span>
                 </a>
             </li>
             <template v-if="section > 1">
                 <li class="page-item">
-                    <a class="page-link" href="#" @click.prevent="swtiched(1)">1</a>
+                    <a class="page-link" href="#" @click.prevent="switched(1)">1</a>
                 </li>
                 <li class="page-item">
                     <a class="page-link" href="#" @click.prevent="goBackASection">...</a>
                 </li>
             </template>
             <li class="page-item" v-for="page in pages" :key="page.id" :class="{ 'active': meta.current_page === page }">
-                <a class="page-link" href="#" @click.prevent="swtiched(page)">{{ page }}</a>
+                <a class="page-link" href="#" @click.prevent="switched(page)">{{ page }}</a>
             </li>
             <template v-if="section < sections">
                 <li class="page-item">
                     <a class="page-link" href="#" @click.prevent="goForwardASection">...</a>
                 </li>
                 <li class="page-item">
-                    <a class="page-link" href="#" @click.prevent="swtiched(meta.last_page)">{{  meta.last_page }}</a>
+                    <a class="page-link" href="#" @click.prevent="switched(meta.last_page)">{{  meta.last_page }}</a>
                 </li>
             </template>
             <li class="page-item" :class="{ 'disabled': meta.current_page === meta.last_page }">
-                <a class="page-link" href="#" aria-label="Next" @click.prevent="swtiched(meta.current_page + 1)">
+                <a class="page-link" href="#" aria-label="Next" @click.prevent="switched(meta.current_page + 1)">
                     <span aria-hidden="true">&raquo;</span>
                     <span class="sr-only">Next</span>
                 </a>
@@ -72,19 +72,19 @@
         },
         methods: {
             goBackASection () {
-                this.swtiched(
+                this.switched(
                     this.firstPageOfSection(this.section - 1)
                 )
             },
             goForwardASection () {
-                this.swtiched(
+                this.switched(
                     this.firstPageOfSection(this.section + 1)
                 )
             },
             firstPageOfSection (section) {
                 return (section - 1) * this.numbersPerSection + 1
             },
-            swtiched (page) {
+            switched (page) {
 
                 if (page <= 0 || page > this.meta.last_page) {
                     return
